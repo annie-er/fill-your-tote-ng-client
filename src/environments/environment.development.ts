9f8dce2b1a505ec7d46356ee4eabffeb3d603794
Annie Rong <annierong302@gmail.com>
2026-05-18 20:41:08 -0400

    chore/stop tracking env file

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080',
-    stripePublishableKey: 'pk_test_51TKR5R7IjJj7BqEUDYldBVt5N04C6YBUZGCwv4wYduCxO8db2kSkmIDFzzyetgHUrirv1dGHFRuQmqcwmBnK9J4t00k1lYia5g',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/api',
-  stripePublishableKey: 'pk_test_51TKR5R7IjJj7BqEUDYldBVt5N04C6YBUZGCwv4wYduCxO8db2kSkmIDFzzyetgHUrirv1dGHFRuQmqcwmBnK9J4t00k1lYia5g',
-};
